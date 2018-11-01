e9ac7e6d458e7be9ca0ab75666a6f44241ab2593
SIDDHARTH <39899291+Siddharth6@users.noreply.github.com>
2018-11-01 20:01:08 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,36 @@
+
+    // Sidenav
+    const sideNav = document.querySelector('.sidenav');
+    M.Sidenav.init(sideNav, {});
+
+    // Slider
+    const slider = document.querySelector('.slider');
+    M.Slider.init(slider, {
+      indicators: false,
+      height: 500,
+      transition: 500,
+      interval: 6000
+    });
+
+    // Autocomplete
+    const ac = document.querySelector('.autocomplete');
+    M.Autocomplete.init(ac, {
+      data: {
+        "HTML":null,
+        "CSS":null,
+        "Google":null,
+        "JAVA":null,
+        "JAVASCRIPT":null,
+        "JQUERY":null,
+      }
+    });
+
+    // Material Boxed
+    const mb = document.querySelectorAll('.materialboxed');
+    M.Materialbox.init(mb, {});
+
+    // ScrollSpy
+    const ss = document.querySelectorAll('.scrollspy');
+    M.ScrollSpy.init(ss, {});
+
+  
